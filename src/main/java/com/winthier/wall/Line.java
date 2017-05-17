6bfa93eb462686c6d61bc55eafb775248a132481
StarTux <ths.loy@googlemail.com>
2017-05-17 13:23:27 +0200

    Remove unused import

--- src/main/java/com/winthier/wall/Line.java
+++ src/main/java/com/winthier/wall/Line.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import org.apache.commons.lang3.text.WordUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.configuration.ConfigurationSection;
