858c005f64374b41c4cdf1a7c33fbbd4c07b3609
StarTux <ths.loy@googlemail.com>
2022-09-04 00:37:53 +0200

    Send on join

--- src/main/java/com/winthier/wall/WallEventListener.java
+++ src/main/java/com/winthier/wall/WallEventListener.java
@@ -3,27 +3,28 @@ package com.winthier.wall;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 import lombok.RequiredArgsConstructor;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
-import org.bukkit.event.player.PlayerResourcePackStatusEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 @RequiredArgsConstructor
 class WallEventListener implements Listener {
     final WallPlugin plugin;
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
-    void onPlayerResourcePackStatus(PlayerResourcePackStatusEvent event) {
-        if (event.getStatus() != PlayerResourcePackStatusEvent.Status.SUCCESSFULLY_LOADED) return;
+    private void onPlayerJoin(PlayerJoinEvent event) {
         final List<String> list = plugin.getJoinWalls();
         if (list == null || list.isEmpty()) return;
         String key = list.get(ThreadLocalRandom.current().nextInt(list.size()));
         final Wall wall = plugin.getWalls().get(key);
         if (wall == null) return;
-        if (!wall.hasPermission(event.getPlayer())) return;
-        wall.send(event.getPlayer());
+        final Player player = event.getPlayer();
+        if (!wall.hasPermission(player)) return;
+        Bukkit.getScheduler().runTaskLater(plugin, () -> wall.send(player), 60L);
     }
 
     @EventHandler
